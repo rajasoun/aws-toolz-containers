2405941f657ba58f5620bfd37abef56ac5c40ae8
Raja Soundaramourty <rajasoun@cisco.com>
2022-07-06 14:06:36 +0530

    feat(initial checkin): assembly of toolz for aws - dev-sec-ops catalogue

--- assist.sh
+++ assist.sh
@@ -8,31 +8,14 @@ GREEN=$'\e[32m'
 BLUE=$'\e[34m'
 ORANGE=$'\x1B[33m'
 
-
-function setup(){
-    for directory in ./* # iterate over all files in current dir
-    do
-        if [ -d "$directory" ] # if it's a directory
-        then
-            cp .ci/template/Makefile "$directory" # copy .ci/template/Makefile into it
-        fi
-    done
-     echo -e "${GREEN}Makefile Injected to all sub-directories successfully${NC}\n" 
-}
-
-function teardown(){
-    find .  -name "Makefile" -not -path "./.ci/*" -delete
-    echo -e "${GREEN}Makefile Deleted from all sub-directories  successfully${NC}\n" 
-}
-
 function build(){
     # Build Base First
-    cd base && make build && cd -
+    cd base && make -f ../.ci/Makefile build && cd -
     for directory in ./* # iterate over all files in current dir
     do
         if [ -d "$directory" ] # if it's a directory
         then
-            cd $directory && make build && cd -
+            cd $directory && make -f ../.ci/Makefile build && cd -
         fi
     done
      echo -e "${GREEN}All Containers Built successfully${NC}\n" 
@@ -43,7 +26,7 @@ function push(){
     do
         if [ -d "$directory" ] # if it's a directory
         then
-            cd $directory && make push && cd -
+            cd $directory && make -f ../.ci/Makefile push && cd -
         fi
     done
      echo -e "${GREEN}All Containers Pushed successfully To Registry${NC}\n" 
@@ -54,7 +37,7 @@ function clean(){
     do
         if [ -d "$directory" ] # if it's a directory
         then
-            cd $directory && make clean && cd -
+            cd $directory && make -f ../.ci/Makefile clean && cd -
         fi
     done
      echo -e "${GREEN}All Containers Removed successfully from loacl system${NC}\n" 
@@ -68,12 +51,6 @@ choice=$( tr '[:upper:]' '[:lower:]' <<<"$opt" )
 echo -e "\n${BLUE}Executing Action : $choice"
 
 case ${choice} in
-    "setup")
-        setup 
-    ;;
-    "teardown")
-        teardown 
-    ;;
     "build")
         build
     ;;
@@ -88,8 +65,6 @@ case ${choice} in
 cat <<-EOF
 Commands:
 ---------
-  setup       -> Inject Makefile to all sub directories from .ci/template
-  teardown    -> Delete Makefile from all sub directories
   build       -> Build all Containers
   push        -> Push all Containers
   clean       -> Clean all Containers
