fa86b93dbc2b2793a773a9e335edcf845e10c133
Raja Soundaramourty <rajasoun@cisco.com>
2022-07-06 14:16:27 +0530

    feat(initial checkin): assembly of toolz for aws - dev-sec-ops catalogue

--- assist.sh
+++ assist.sh
@@ -9,6 +9,12 @@ BLUE=$'\e[34m'
 ORANGE=$'\x1B[33m'
 
 function build(){
+    directory=$1
+    if [ $directory != "" ];then 
+        cd $directory && make -f ../.ci/Makefile build && cd -
+        return 0
+    fi 
+
     # Build Base First
     cd base && make -f ../.ci/Makefile build && cd -
     for directory in ./* # iterate over all files in current dir
@@ -17,11 +23,18 @@ function build(){
         then
             cd $directory && make -f ../.ci/Makefile build && cd -
         fi
-    done
-     echo -e "${GREEN}All Containers Built successfully${NC}\n" 
+    done 
+    cd .cd/assembly && make build && cd -
+    echo -e "${GREEN}All Containers Built successfully${NC}\n"
 }
 
 function push(){
+    directory=$1
+    if [ $directory != "" ];then 
+        cd $directory && make -f ../.ci/Makefile push && cd -
+        return 0
+    fi 
+
     for directory in ./* # iterate over all files in current dir
     do
         if [ -d "$directory" ] # if it's a directory
@@ -29,10 +42,17 @@ function push(){
             cd $directory && make -f ../.ci/Makefile push && cd -
         fi
     done
-     echo -e "${GREEN}All Containers Pushed successfully To Registry${NC}\n" 
+    cd .cd/assembly && make push && cd -
+    echo -e "${GREEN}All Containers Pushed successfully To Registry${NC}\n" 
 }
 
 function clean(){
+    directory=$1
+    if [ $directory != "" ];then 
+        cd $directory && make -f ../.ci/Makefile clean && cd -
+        return 0
+    fi 
+
     for directory in ./* # iterate over all files in current dir
     do
         if [ -d "$directory" ] # if it's a directory
@@ -40,29 +60,33 @@ function clean(){
             cd $directory && make -f ../.ci/Makefile clean && cd -
         fi
     done
-     echo -e "${GREEN}All Containers Removed successfully from loacl system${NC}\n" 
+    cd .cd/assembly && make push && cd -
+    echo -e "${GREEN}All Containers Removed successfully from loacl system${NC}\n" 
 }
 
 
 
 opt="$1"
+dir_path="$2"
 choice=$( tr '[:upper:]' '[:lower:]' <<<"$opt" )
 
 echo -e "\n${BLUE}Executing Action : $choice"
 
 case ${choice} in
     "build")
-        build
+        build  $dir_path
     ;;
     "push")
-        push
+        push   $dir_path
     ;;
     "clean")
-        clean
+        clean  $dir_path
     ;;
     *)
-    echo "${RED}Usage: assist.sh < setup | teardown >  ${NC}"
+    echo "${RED}Usage: assist.sh < build | push | clean > [dir_path] ${NC}"
+    echo "${ORANGE}When dir_path is not povided actions runs on all dirs${NC}"
 cat <<-EOF
+
 Commands:
 ---------
   build       -> Build all Containers
