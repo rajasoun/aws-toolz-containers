2790351d63d3680112ee00c4cb2963bde43be27d
Raja Soundaramourty <rajasoun@cisco.com>
2022-07-08 23:25:23 +0530

    feat(initial checkin): assembly of toolz for aws - dev-sec-ops catalogue

--- shell.sh
+++ shell.sh
@@ -5,6 +5,10 @@ SCRIPT_PATH="$SCRIPT_DIR/workspace-scripts/automator/libs/os.sh"
 # shellcheck source=/dev/null
 source "$SCRIPT_PATH"
 
+# VERSION=$(git describe --tags --abbrev=0 | sed -Ee 's/^v|-.*//')
+export name="rajasoun/aws-toolz-assembly"
+export VERSION=1.0.0
+
 # Workaround for Path Limitations in Windows
 function _docker() {
   export MSYS_NO_PATHCONV=1
@@ -53,21 +57,29 @@ function launch(){
             "$name:$VERSION"
 }
 
-name=${1-"rajasoun/aws-toolz-assembly"}
-VERSION=${2-"1.0.0"}
-ENV=${3-"dev"}
-ENTRY_POINT_CMD=${4-"/bin/zsh"}
+function setup(){
+    ENV=$1
+    if [ "$ENV" = "dev" ]; then
+        echo "$(date)" > "$(git rev-parse --show-toplevel)/.dev"
+        echo -e "\n${BOLD}${UNDERLINE}CI Shell For Dev${NC}"
+        _configure_ssh_gitconfig
+        make -f .config/Makefile tools-prerequisite
+        rm -fr "$(date)" > "$(git rev-parse --show-toplevel)/.ops"
+    else
+        echo -e "\n${BOLD}${UNDERLINE}CI Shell For Ops${NC}"
+        _git_config
+    fi
+    if [ -z $ENTRY_POINT_CMD ]; then
+        ENTRY_POINT_CMD="/bin/zsh"
+    fi
+}
 
-if [ "$ENV" = "dev" ]; then
-    echo "$(date)" > "$(git rev-parse --show-toplevel)/.dev"
-    echo -e "\n${BOLD}${UNDERLINE}CI Shell For Dev${NC}"
-    _configure_ssh_gitconfig
-    make -f .config/Makefile tools-prerequisite
-    rm -fr "$(date)" > "$(git rev-parse --show-toplevel)/.ops"
-else
-    echo -e "\n${BOLD}${UNDERLINE}CI Shell For Ops${NC}"
-    _git_config
-fi
+function main(){
+    ENV=$1
+    ENTRY_POINT_CMD=$2
 
-launch $ENTRY_POINT_CMD
+    setup $ENV
+    launch $ENTRY_POINT_CMD
+}
 
+main $@
