16d4a105227e400aedc43842e098183f107738c5
Raja Soundaramourty <rajasoun@cisco.com>
2022-07-18 13:28:45 +0530

    feat(packer): packer  tooling

--- workspace-scripts/tests/system/e2e_tests.sh
+++ workspace-scripts/tests/system/e2e_tests.sh
@@ -28,6 +28,7 @@ check "fetch" fetch --version
 check "aws-sso" aws-sso -version
 check "terraform" terraform version
 check "terragrunt" terragrunt --version
+check "packer" packer version
 check "pulumi" pulumi version
 check "http" http --version
 
