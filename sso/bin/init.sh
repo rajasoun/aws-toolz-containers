91e6ff25a7cf77f6e8d029f9083237fbf42a3c56
Raja Soundaramourty <rajasoun@icloud.com>
2022-10-04 12:20:19 +0530

    git ssh support

--- sso/bin/init.sh
+++ sso/bin/init.sh
@@ -7,10 +7,10 @@ function check_vpn_connection() {
   server=$1
   port=$2
   if nc -z "$server" "$port" 2>/dev/null; then
-    echo -e "${GREEN}VPN Connection $server Passed ✅ ${NC}\n"
+    echo -e "VPN Connection $server Passed ✅ \n"
     return 0
   else
-    echo -e "${RED}VPN Connection $server Failed ❌ ${NC}\n"
+    echo -e "VPN Connection $server Failed ❌ \n"
     return 1
   fi
 }
@@ -32,12 +32,19 @@ function build_linux_bin(){
     cd -
 }
 
+function clean(){
+    rm -fr duo-sso
+}
+
 function main(){
     if [ ! -f bin/aws-sso ];then 
         check_vpn_connection "$VPN_GIT_HOST" 443 || return 1
         git_clone
         build_linux_bin
         mv duo-sso/build/aws-sso bin/aws-sso
+        clean
+    else 
+        echo -e "aws-sso binary exists"
     fi 
 }
 
