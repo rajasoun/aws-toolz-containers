9b9237787a96fcc22be0613a4a0c592170052e57
Raja Soundaramourty <rajasoun@icloud.com>
2022-10-07 08:46:23 +0000

    kube config

--- .config/.kube/init.sh
+++ .config/.kube/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export cluster=eks
+export cluster=Development
 export region=us-east-1
 export account_id=$(aws sts get-caller-identity --output text --query 'Account')
 
 
-aws eks update-kubeconfig --region $region --name $cluster
+aws eks update-kubeconfig --region $region  --name $cluster
 export KUBECONFIG=$KUBECONFIG:~/.kube/config
