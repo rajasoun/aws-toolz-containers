9c6302cb39e2bf8fd260ebe5990b27f74b088a40
Raja Soundaramourty <rajasoun@icloud.com>
2022-10-10 09:08:03 +0000

    version upgrade for terraform packer and pulumi

--- workspace-scripts/tests/system/e2e_tests.sh
+++ workspace-scripts/tests/system/e2e_tests.sh
@@ -9,27 +9,42 @@ checkCommon
 
 # Definition specific tests
 #checkExtension "ms-azuretools.vscode-docker"
-check "pre-commit" pre-commit --version
+
 check "gh" gh --version
+check "fetch" fetch --version
+
+check "pre-commit" pre-commit --version
 check "dotenv" dotenv --version
+
+check "aws" aws --version
+check "awless" awless --version
+
+check "aws-sso" aws-sso -version
 check "gpg2" gpg2 --version
 check "pass" pass --version
-check "sentry-cli" sentry-cli --version
-check "syft" syft --version
-check "grype" grype version
-check "aws" aws --version
 check "aws-vault" aws-vault --version
-check "awless" awless --version
+
+check "aws-crawl" aws-crawl version
 check "awsaudit" awsaudit --version
 check "cloudsplaining" cloudsplaining --version
+
+check "syft" syft --version
+check "grype" grype version
+
 check "cloud-nuke" cloud-nuke --version
 check "aws-nuke" aws-nuke version
-check "fetch" fetch --version
-check "aws-sso" aws-sso -version
+
+check "packer" packer version
 check "terraform" terraform version
 check "terragrunt" terragrunt --version
-check "packer" packer version
+
+check "aws-iam-authenticator" aws-iam-authenticator version
+check "kubectl" kubectl version  --client --output=yaml
+check "eksctl" eksctl version
+check "helm" helm version
+
 check "pulumi" pulumi version
+check "sentry-cli" sentry-cli --version
 check "http" http --version
 
 #check "pre-commit" pre-commit run --all-files
