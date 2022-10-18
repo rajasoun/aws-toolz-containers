b7f2691d7a74425d0a6a2b98587aea367bf15bd0
Raja Soundaramourty <rajasoun@icloud.com>
2022-10-18 07:48:23 +0000

    fix kubectl init script

--- .config/.kube/init.sh
+++ .config/.kube/init.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-AWS_PROFILE=${1:-"lcce-development"}
-AWS_REGION=${2:-"us-east-1"}
-CLUSTER=${3:-"Development"}
+function do_config(){
+    AWS_PROFILE=${1:-"lcce-development"}
+    AWS_REGION=${2:-"us-east-1"}
+    CLUSTER=${3:-"Development"}
 
+    echo -e "export AWS_PROFILE=$AWS_PROFILE" >  .config/.kube/eks.env
+    echo -e "export AWS_REGION=$AWS_REGION"   >> .config/.kube/eks.env
+    echo -e "export CLUSTER=$CLUSTER"          >> .config/.kube/eks.env
+    echo -e "export KUBECONFIG=$KUBECONFIG:~/.kube/config" >> .config/.kube/eks.env
+}
+
+do_config $@
+export $(grep -v '^#' .config/.kube/eks.env | xargs)
 aws eks update-kubeconfig --region $AWS_REGION  --name $CLUSTER
-
-export AWS_PROFILE
-export AWS_REGION
-export CLUSTER
-export KUBECONFIG=$KUBECONFIG:~/.kube/config
--- workspace-scripts/automator/libs/aws.sh
+++ workspace-scripts/automator/libs/aws.sh
@@ -174,47 +174,6 @@ function profile_aws_account(){
     awless list elasticips
 }
 
-function aws_profile_config() {
-  if [ ! -f ~/.aws/config ]; then
-    echo -e "You must have AWS profiles set up in ~/.aws/config"
-    return 1
-  fi
-
-  local list=$(grep '^[[]profile' <~/.aws/config | awk '{print $2}' | sed 's/]$//')
-  if [[ -z $list ]]; then
-    echo -e "You must have AWS profiles set up in ~/.aws/config"
-    return 1
-  fi
-
-  # if AWS_PROFILE is empty - Interative Mode
-  if [ -z "$AWS_PROFILE" ];then
-    local nlist=$(echo "$list" | nl)
-    while [[ -z $AWS_PROFILE ]]; do
-        export AWS_PROFILE=$(read -p "AWS profile? `echo $'\n\r'`$nlist `echo $'\n> '`" N; echo "$list" | sed -n ${N}p)
-    done
-  fi
-}
-
-function kube_config(){
-    export cluster=Development
-    export region=us-east-1
-    export account_id=$(aws sts get-caller-identity --output text --query 'Account')
-
-
-    aws eks update-kubeconfig --region $region  --name $cluster
-    export KUBECONFIG=$KUBECONFIG:~/.kube/config
-}
-
-function set_env(){
-    aws-sso
-    aws_profile_config
-    kube_config
-    whoami
-    echo -e "export AWS_PROFILE=$AWS_PROFILE" > .config/.aws/.env
-    echo -e "export KUBECONFIG=$KUBECONFIG:~/.kube/config" >> .config/.aws/.env
-    source .config/.aws/.env
-}
-
 function main(){
     set_env
     list_aws_resources
